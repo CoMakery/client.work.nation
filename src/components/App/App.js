8fdf87b7ba901f3058359318407c1005ee99ff63
Kyle Leimetter <thinkin3d@gmail.com>
2017-04-25 02:07:13 -0700

    confirm page styling begin

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,11 +23,15 @@ export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <Link to="/"><h1>Work.nation</h1></Link>
-          {this.menu()}
-          <hr />
-          {this.routes()}
+        <div className="page-wrapper">
+          <div className="row">
+            <div className="small-8 columns">
+              <Link to="/"><h1 className="project-name">work.nation</h1></Link>
+              {this.routes()}
+            </div>
+            <div className="small-3 columns" />
+            <div className="small-1 columns">{this.menu()}</div>
+          </div>
         </div>
       </BrowserRouter>
     )
@@ -37,7 +41,7 @@ export default class App extends React.Component {
     if (this.state.currentUser) {   // need to store this in state to get it to render properly
       return <ul className="menu vertical">
         <li><Link to={'/profile/' + this.state.currentUser}>[Avatar Image]</Link></li>
-        <li><Link to="/confirm"><img src="/static/images/icon_confirm.png" /></Link></li>
+        <li><Link to="/confirm"><img src="/static/images/icon_confirmed.svg" /></Link></li>
         <li><Link to="/search"><img src="/static/images/icon_Search.png" /></Link></li>
         <li><Link to="/project"><img src="/static/images/icon_ProjSetup.png" /></Link></li>
         <li><Link to="/logout">[Logout Icon]</Link></li>
@@ -83,4 +87,3 @@ export default class App extends React.Component {
     </div>
   )
 }
-
