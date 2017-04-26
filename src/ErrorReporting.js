8433cd32e28bf3cafddd865fb2e178d9a1d27c17
Harlan T Wood <dev+harlan@comakery.com>
2017-04-26 00:10:53 -0700

    Set up sentry.io error reporting via raven module

---
+++ src/ErrorReporting.js
@@ -0,0 +1,13 @@
+// Set up Raven ( sentry.io ) error reporter as early as possible:
+
+if (process.env.NODE_ENV === 'production') {
+  const Raven = require('raven-js')
+
+  const raven = Raven.config('https://0fbb46c5ecb54c63ac8a84db7c7c17e3@sentry.io/161843', {
+    environment: process.env.NODE_ENV,
+    debug: true,
+    // more options here: https://docs.sentry.io/clients/javascript/config/
+  })
+
+  raven.install()
+}
