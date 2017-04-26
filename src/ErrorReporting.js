2cd3453241cf50ba8ee8f2611f8ab2ed7b9b6c0d
Harlan T Wood <dev+harlan@comakery.com>
2017-04-25 23:04:00 -0700

    prod

--- src/ErrorReporting.js
+++ src/ErrorReporting.js
@@ -1,10 +1,13 @@
 // Set up Raven ( sentry.io ) error reporter as early as possible:
-import Raven from 'raven-js'
 
-const raven = Raven.config('https://0fbb46c5ecb54c63ac8a84db7c7c17e3@sentry.io/161843', {
-  environment: process.env.NODE_ENV,
-  debug: true,
-  // more options here: https://docs.sentry.io/clients/javascript/config/
-})
+if (process.env.NODE_ENV === 'production') {
+  const Raven = require('raven-js')
 
-raven.install()
+  const raven = Raven.config('https://0fbb46c5ecb54c63ac8a84db7c7c17e3@sentry.io/161843', {
+    environment: process.env.NODE_ENV,
+    debug: true,
+    // more options here: https://docs.sentry.io/clients/javascript/config/
+  })
+
+  raven.install()
+}
