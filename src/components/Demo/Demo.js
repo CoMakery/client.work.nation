2015b9b33baa110d0a92931f6281c28f43ba6973
Harlan T Wood <dev+harlan@comakery.com>
2017-05-02 23:59:08 -0700

    Update staging logins; demo button tweaks

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -13,19 +13,23 @@ export default class Login extends React.Component {
   render() {
     return (
       <div className="login-buttons">
-        <div className="button" onClick={this.demoLogin} data-uport="0xa778edb246f6def1c51e599c23e7c8f1d1493633">
-            Login as 0xa778edb246f6def1c51e599c23e7c8f1d1493633
-          </div>
-        <hr />
-        <div className="button" onClick={this.demoLogin} data-uport="0xa778edb246f6def1c51e599c23e7c8f1d1493633">
-            Login as 0xa778edb246f6def1c51e599c23e7c8f1d1493633
-          </div>
-        <hr />
-        <div className="button" onClick={this.demoLogin} data-uport="0xe9e77f329190eb415e09050183b79c859572659b">
-            Login as 0xe9e77f329190eb415e09050183b79c859572659b
+        <br />
+        <div className="button expand" onClick={this.demoLogin} data-uport="0x75e9b52ac5fec235d8dcec5cb3d9623390c7eefd">
+          Staging: Login as 0x75e9b52ac5fec235d8dcec5cb3d9623390c7eefd
         </div>
-        <hr />
-        <div className="button" onClick={this.demoLogin} data-uport="0x01d3b5eaa2e305a1553f0e2612353c94e597449e">
+        <br />
+        <br />
+        <div className="button expand" onClick={this.demoLogin} data-uport="0xc0967d2f86d02d0c8b9ebb6de7dc00ef6f18f666">
+          Staging: Login as 0xc0967d2f86d02d0c8b9ebb6de7dc00ef6f18f666
+        </div>
+        <br />
+        <br />
+        <div className="button expand" onClick={this.demoLogin} data-uport="0xe9e77f329190eb415e09050183b79c859572659b">
+          Dev: Login as 0xe9e77f329190eb415e09050183b79c859572659b
+        </div>
+        <br />
+        <br />
+        <div className="button expand" onClick={this.demoLogin} data-uport="0x01d3b5eaa2e305a1553f0e2612353c94e597449e">
           Login as Harlan
         </div>
       </div>
