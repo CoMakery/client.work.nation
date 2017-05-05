2e87a7e7c903d8704e0b03469d6d9816172a7f68
Harlan T Wood <dev+harlan@comakery.com>
2017-05-04 21:34:16 -0700

    Comment out unused header

--- src/models/Server.js
+++ src/models/Server.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
   // baseURL: process.env.REACT_APP_API_SERVER,
-  headers: {'X-Key-Inflection': 'camel'},  // camel case keys
+  // headers: {'X-Key-Inflection': 'camel'},  // camel case keys // created by Olive Branch on server side, not currently in use
   timeout: 30000,
 })
 
