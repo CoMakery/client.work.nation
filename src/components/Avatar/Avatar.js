e47fd8894757bafb9aa5f9a35f557e83a6fdc8ff
Noah Thorp <noah@comakery.com>
2017-05-05 10:47:33 -0700

    add alt tag and use name

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default ({avatarImageIpfsKey, name}) => {
   d({avatarImageIpfsKey})
   if (avatarImageIpfsKey && !avatarImageIpfsKey.match(/^QmAVATAR/)) {
-    return <img src={'//ipfs.io/ipfs/' + avatarImageIpfsKey} className="profile-photo" />
+    return <img src={'//ipfs.io/ipfs/' + avatarImageIpfsKey} className="profile-photo" alt={name} />
   } else {
     // return <img src={`https://robohash.org/${name || avatarImageIpfsKey}.png?size=150x150`} className="profile-photo" />
     return <img src="/static/images/icon_blank_avatar.svg" className="profile-photo" />
